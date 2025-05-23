77838f94f9c025f1239d38708ee7dc28c5c0200f
AlexisJr2004 <duranalexis879@gemail.com>
2025-05-23 18:30:07 -0500

    Calculadora Matricial V4.5.0

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from flaskwebgui import FlaskUI
 from flask import Flask, render_template
 from routes import (
     matrix_bp, polynomial_bp, vector_bp, plot_bp, calculus_bp, differential_bp, epidemic_bp, eigen_bp, random_bp, simulate_bp, montecarlo_bp
@@ -22,11 +21,5 @@ app.register_blueprint(montecarlo_bp)
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    FlaskUI(
-        app=app,
-        server="flask",
-        width=800,
-        height=600,
-        fullscreen=False,
-    ).run()
+if __name__ == '__main__':
+    app.run(debug=True)
